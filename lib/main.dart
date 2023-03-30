4f9933abd875a734bedc100c046449099400c099
poowamin <james.poowamin7@gmail.com>
2023-03-30 17:08:53 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './provider/great_places.dart';
+import './screens/place_list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+      ),
+    );
+  }
+}
+
+class ChangeNotifireProvider {}
